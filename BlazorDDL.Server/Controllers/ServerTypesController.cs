5fbe1093933b262b109d662b9238e76a1e951bb7
Ying Jin <yingjin.us@gmail.com>
2019-03-28 13:37:11 -0400

    Adding the web API Controller to the Application in Server project

---
+++ BlazorDDL.Server/Controllers/ServerTypesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDDL.Server.DataAcces;
+using BlazorDDL.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorDDL.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class ServerTypesController : Controller
+    {
+        DataAccessLayer objDal = new DataAccessLayer();
+        // GET: api/<controller>
+        [HttpGet]
+        [Route("api/ServerTypes/GetServerTypesList")]
+        public IEnumerable<ServerTypes> GetServerTypesList()
+        {
+            return objDal.GetAllServerTypes();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet]
+        [Route("api/ServerTypes/GetServers")]
+        public IEnumerable<Servers> GetServers(string id)
+        {
+            return objDal.GetServersData(id);
+        }
+    }
+}
