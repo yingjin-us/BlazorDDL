eb6e4931c60404fe751cd0383504ccbd10e571c7
Ying Jin <yingjin.us@gmail.com>
2019-03-28 13:30:02 -0400

    Creating the Data Access Layer for the Application in Server project

---
+++ BlazorDDL.Server/DataAccess/DataAccessLayer.cs
@@ -0,0 +1,40 @@
+﻿using BlazorDDL.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDDL.Server.DataAcces
+{
+    public class DataAccessLayer
+    {
+        ApplicationInventoryContext db = new ApplicationInventoryContext();
+
+        public IEnumerable<ServerTypes> GetAllServerTypes()
+        {
+            try
+            {
+                return db.ServerTypes.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public IEnumerable<Servers> GetServersData(string id)
+        {
+            try
+            {
+                List<Servers> lstCity = new List<Servers>();
+                lstCity = (from ServerName in db.Servers where ServerName.ServerTypeId == Int64.Parse(id) select ServerName).ToList();
+
+                return lstCity;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Blazor Cascading Download
+# Blazor Cascading Dropdown
 https://medium.freecodecamp.org/how-to-create-a-cascading-dropdownlist-in-blazor-using-ef-core-d230bb5bff5f
